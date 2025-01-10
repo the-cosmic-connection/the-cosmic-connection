d44ac62a3d230c1b93fe5a99e3c9a486caffe444
Arnab Biswas <arnabworkspace07@gmail.com>
2025-01-10 23:28:05 +0530

    first commit

---
+++ send-email.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize and retrieve form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Recipient email address
+    $to = "arnabworkspace07@gmail.com";
+    $subject = "Contact Form Submission from $name";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "You have received a new message from your website contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send the message. Please try again.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
+
